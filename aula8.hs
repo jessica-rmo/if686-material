1273515885c3d570c1fb47226342bdf7252e3afe
jessica-rmo <jrmo@cin.ufpe.br>
2018-09-16 20:12:16 -0300

    atualizando aula8 #5

--- aula8.hs
+++ aula8.hs
@@ -1,5 +1,16 @@
--- Filter @TO_DO pegar o que tem no final da aula7.hs]
+-- Filter
+
+filtro :: (a -> Bool) -> [a] -> [a]
+filtro f [] = []
+filtro f (x:xs)
+    | f x       = x : filtro f xs
+    | otherwise = filtro f xs
 
 -- Composicao de funcoes
+
+twice :: (t -> t) -> (t -> t)
+twice f = f . f
+-- (twice succ) 12 = (succ . succ) 12 = succ (succ 12) = 14
+
 
 -- @TO_DO completar com código da aula postado no Piazza
