932bbd6e084fce6bdd2a394bca5a9dea81a3f81c
jessica-rmo <jrmo@cin.ufpe.br>
2018-10-11 02:49:34 -0300

    consertando as questoes ee1 2018.1

--- monitoria/monitoria27-09.hs
+++ monitoria/monitoria27-09.hs
@@ -6,7 +6,7 @@ import Data.List (sort)
 f = filter (>5) . map (+2)
 -- f [1..5]
 -- [6,7]
--- criar uma função g tal que g = filter s1 . map s2 tal que seja equivalente a f
+-- criar uma função g tal que g = map s1 . filter s2 tal que seja equivalente a f
 
 g = map(+2) . filter (>3)
 
@@ -20,15 +20,21 @@ getInts :: CInt -> [Int]
 getInts (Conjunto a) = a  --a é a lista de Int
 
 makeSet :: [Int] -> CInt
-makeSet (x:xs) = if x `notElem` xs then x:l else l
-    where l = getInts (makeSet xs)
+makeSet [] = Conjunto []
+makeSet (x:xs) = if x `notElem` xs
+                   then Conjunto (x:l)
+                   else Conjunto l
+       where l = getInts (makeSet xs)
 
 -- usando sort para organizar a lista resultante
 makeSet2 :: CInt -> [Int]
-makeSet2 [] = []
-makeSet2 (x:xs) = Conjunto . sort $ if x `notElem` xs then x:l else l
-    where l = getInts (makeSet xs)
+makeSet2 (Conjunto []) = []
+makeSet2 (Conjunto (x:xs)) = sort $ if x `notElem` xs
+                                      then (x:l)
+                                      else l
+        where l = getInts (makeSet xs)
 
 --b)
+
 
 --c)
