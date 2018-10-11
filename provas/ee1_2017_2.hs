dbc225f262fe43fb135215ba3533fb240b1e4d8f
Jessica Raquel de Melo Oliveira <jrmo@ug1c44.windows.cin.ufpe.br>
2018-10-11 14:32:51 -0300

    respostas da ee1 2017.2

--- provas/ee1_2017_2.hs
+++ provas/ee1_2017_2.hs
@@ -1,6 +1,41 @@
--- EE1 2017.2
+-- EE1 PLC 2017.2
 
--- Questao 4
+--q1
+sublistas :: [a] -> [[a]]
+sublistas [] = [[]]
+sublistas (x:xs) = [(x:z) | z <- sublistas xs] ++ sublistas xs
+
+--q2  --OBS: funciona, mas não usa a função filter de haskell, como pedia a questão
+filtrarEInserir :: [[Int]] -> Int -> ([[Int]], Int)
+filtrarEInserir l n = (arg1 l, ((arg2 (arg1 l))*n) )
+
+arg1 :: [[Int]] -> [[Int]]
+arg1 [] = []
+arg1 (x:xs) =
+   if (soma odd x > soma even x)
+   then (x : arg1 xs)
+   else (arg1 xs)
+
+arg2 :: [[Int]] -> Int
+arg2 [] = 0
+arg2 [a] = soma odd a
+arg2 (x:(y:ys))
+   | soma odd x > soma odd y = arg2 (x:ys)
+   | otherwise               = arg2 (y:ys)
+
+somaAux :: [Int] -> Int
+somaAux [] = 0
+somaAux (x:xs) = x + somaAux xs
+
+soma :: (Int->Bool) -> [Int] -> Int
+soma f [] = 0
+soma f l = somaAux (filter f l)
+
+--q3
+altMap f1 f2 [] = []
+altMap f1 f2 (x:xs) = f1 x : (altMap f2 f1 xs)
+
+-- q4
 {- (a)
 Defina a função poli :: Integer −> Integer −> Integer −> Integer −> Integer que recebe como argumentos os coeficientes de uma funcao polinomial de grau 2 e devolve uma funcao de nteiro para inteiro (um polinomio) -}
 
@@ -32,3 +67,13 @@ appListaPoli _ [] = []
 appListaPoli (f:fs) (x:xs) = f x : appListaPoli fs xs
 -}
 
+--q5
+data Mobile = Pendente Int | Barra Mobile Mobile
+--a)
+peso :: Mobile -> Int
+peso (Pendente p) = p
+peso (Barra m1 m2) = peso m1 + peso m2
+--b)
+balanceado :: Mobile -> Bool
+balanceado (Pendente _) = True
+balanceado (Barra m1 m2) = peso m1 == peso m2
