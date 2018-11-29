ad2a479cade2bfbc0ff466d067f3e83995af3e49
jessica-rmo <jrmo@cin.ufpe.br>
2018-11-28 21:16:32 -0300

    exercícios para ee2

---
+++ exercicios/ee2/Vector.java
@@ -0,0 +1,28 @@
+public static class Vector {
+        private final int[] content;
+
+        public Vector(int size) {
+            this.content = new int[size];
+        }
+
+        public int get(int index) {
+            synchronized (this.content) {
+                return this.content[index];
+            }
+        }
+
+        public void set(int index, int value) {
+            synchronized (this.content) {
+                this.content[index] = value;
+            }
+        }
+
+        public void swap(int a, int b) {
+            synchronized (this.content) {
+                int i = this.content[a];
+                this.content[a] = this.content[b];
+                this.content[b] = i;
+            }
+        }
+    }
+}
