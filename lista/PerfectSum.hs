de8b5c1b262f6533cb7552d7b27f7fa23c999938
jessica-rmo <jrmo@cin.ufpe.br>
2018-12-03 02:49:53 -0300

    q8 76% :\

---
+++ lista/PerfectSum.hs
@@ -0,0 +1,34 @@
+import System.IO
+import Data.List (subsequences)
+import Data.String (words)
+
+perfectSum :: String -> Int -> Int
+perfectSum l n = perfectSumAux (toIntList l) n
+
+perfectSumAux :: [Int] -> Int -> Int
+perfectSumAux [] _ = 0
+perfectSumAux [x] n = if (x==n) then 1 else 0
+perfectSumAux l n = getSubsets l n
+
+toIntList :: String -> [Int]
+toIntList str = map (read) (words str)
+
+getSubsets :: [Int] -> Int -> Int
+getSubsets [] n = 0
+getSubsets [x] n = if (x==n) then 1 else 0
+getSubsets l n = length [ x | x <- (sums (subsets l)), x==n]
+
+subsets :: [Int] -> [[Int]]
+subsets l = subsequences l
+
+sums :: [[Int]] -> [Int]
+sums [[]] = []
+sums [[x]] = [x]
+sums l = [foldr (+) 0 n | n <- l]
+
+
+main :: IO ()
+main = do
+    list <- getLine
+    num <- getLine
+    putStrLn $ show $ perfectSum list (read num)
