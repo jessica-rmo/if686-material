be5dfbc56cb0909255245dc5bbda2e1cbbced899
jessica-rmo <jrmo@cin.ufpe.br>
2018-12-05 15:31:54 -0300

    q6 some changes, still runtime error

--- lista/Pokemon.hs
+++ lista/Pokemon.hs
@@ -5,8 +5,7 @@ catch = do
     min <- getLine
     max <- getLine
     pokemons <- getPokemons
-    let ashPokemons = canCatch (read min) (read max) pokemons
-    list ashPokemons
+    list $ canCatch (read min) (read max) pokemons
 
 getPokemons :: IO [(String,Int)]
 getPokemons = do
@@ -16,17 +15,17 @@ getPokemons = do
     if end then
         return [(name, read power)]
     else do
-        newPokemon <- getPokemons
+        newPokemon <- getPokemons 
         return ((name, read power):newPokemon)
 
-canCatch :: Int -> Int -> [(String,Int)] -> [String]
-canCatch min max l = [ n ++" ("++ show p ++")" | (n,p) <- l, p>=min, p<=max]
+canCatch :: Int -> Int -> [(String,Int)] -> [(String,Int)]
+canCatch min max l = filter (\(name,pw) -> pw>=min && pw<=max) l
 
-list :: [String] -> IO ()
+list :: [(String,Int)] -> IO ()
 list [] = do return ()
-list [x] = do putStrLn x
-list (x:xs) = do putStrLn x
-                 list xs
+list (x@(name,power):xs) = do
+    putStrLn (name ++" ("++ show power ++")")
+    list xs
 
 main :: IO ()
 main = do catch
